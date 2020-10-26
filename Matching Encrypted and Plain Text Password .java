e58314bbb79900b2e7b3634555077e00b79165e4
Mohamed El Ghazali <48661966+mohamedelghazali@users.noreply.github.com>
2020-10-26 05:06:25 +0100

    Delete Matching Encrypted and Plain Text Password .java

--- Matching Encrypted and Plain Text Password .java
+++
@@ -1,6 +0,0 @@
-private void checkPass(String plainPassword, String hashedPassword) {
-		if (BCrypt.checkpw(plainPassword, hashedPassword))
-			System.out.println("The password matches.");
-		else
-			System.out.println("The password does not match.");
-	}
